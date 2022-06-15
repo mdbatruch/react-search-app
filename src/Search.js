0473f084cb1c776faf167ac468f01c382a791000
mdbatruch <mdbatruch@gmail.com>
2022-06-14 23:28:26 -0400

    update Search component

--- src/Search.js
+++ src/Search.js
@@ -1,86 +1,110 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import Suggestions from './Suggestions'
+import React, { useState, useEffect } from 'react'
 import { FaSpinner } from 'react-icons/fa'
 
-const API_URL = 'http://localhost:8888/oop_site/api/read.php';
+      function Search() {
 
-class Search extends Component {
- state = {
-   query: '',
-   isActive: null,
-   icon: false,
-   results: []
-}
+        const URL = 'https://castlegames.mike-batruch.ca';
 
- getInfo = () => {
-    (async() => {
-        let data = null;
-        try {
-          data = await axios.get(`http://localhost:8888/oop_site/api/read_one.php?name=${this.state.query}`);
-        //   data = await axios.get(`http://localhost:8888/oop_site/api/read.php`);
-        } catch (err) {
-          console.error("Error response:");
-          console.error(err.response.data);    // ***
-          console.error(err.response.status);  // ***
-          console.error(err.response.headers); // ***
-          this.setState({results: []})
-        } finally {
-        //   console.log(apiRes);
-        if (data) {
-            this.setState({
-                    results: data.data,
-                    isLoading: false,
-                    icon: false
-                })
-            } else {
-                this.setState({
-                    results: null,
-                    icon: true,
-                })
-            }
-        }
-      })();
-  }
+        const IMAGE_PATH = 'https://castlegames.mike-batruch.ca/images/';
+        const PRODUCT_PATH = 'https://castlegames.mike-batruch.ca/product?id=';
 
- 
-  handleInputChange = () => {
-    this.setState({
-      query: this.search.value
-    }, () => {
-      if (this.state.query) {
-        this.setState({
-            isActive: 1,
-            icon: true
-        })
-          this.getInfo()
-      } else if (!this.state.query || this.state.query == "") {
-        this.setState({
-            isActive: null,
-            icon: false
-        })
+        const [error, setError] = useState(null);
+        const [active, setIsActive] = useState(false);
+        const [icon, setIcon] = useState(false);
+        const [isLoaded, setIsLoaded] = useState(false);
+        const [products, setProducts] = useState([]);
+
+        const [q, setQ] = useState("");
+
+        const [searchParam] = useState(["name"]);
+
+        useEffect(() => {
+            fetch(`${URL}/api/read.php`)
+                .then((res) => res.json())
+                .then(
+                    (result) => {
+                        setIsLoaded(true);
+                        setProducts(result.records);
+                    },
+                    (error) => {
+                        setIsLoaded(true);
+                        setError(error);
+                    }
+                );
+        }, []);
+
+      function search(items) {
+
+          return items.filter((item) => {
+              return searchParam.some((newItem) => {
+
+                  return (
+                      item[newItem]
+                          .toString()
+                          .toLowerCase()
+                          .indexOf(q.toLowerCase()) > -1
+                  );
+
+              });
+          });
       }
-    })
+
+      function handleChange(e) {
+        setQ(e.target.value);
+        setIsActive(true);
+        setIcon(true);
+
+        if (e.target.value.length < 1) {
+          setIsActive(false);
+          setIcon(false);
+        }
+      }
+
+      if (error) {
+          return <>{error.message}</>;
+      } else if (!isLoaded) {
+          return <>loading...</>;
+      } else {
+        console.log(products)
+          return (
+            <form>
+                <div className="input-container">
+                    <input
+                        type="text"
+                        placeholder="Search for..."
+                        value={q}
+                        onChange={handleChange}
+                    />
+                    <FaSpinner icon="spinner" className={icon ? 'show spinner' : 'hide spinner'} />
+                    <div id="container">
+                      <ul id="results" className={active ? 'show' : 'hide'} >
+                        {search(products).length > 0 ? search(products).map((item) => (
+                            <li>
+                              <span>{item.name}</span>
+                              <div class="img-container">
+                                <div class="image">
+                                    <img src={IMAGE_PATH + item.image} class="img-fluid" alt={item.name}/>
+                                </div>
+                            </div>
+                              <div class="item-meta">
+                                <a href={PRODUCT_PATH + item.id} class='product-link title'>
+                                    <div class='name mb-1'>
+                                      {item.name}
+                                    </div>
+                                </a>
+                                <div class='mb-1 price'>
+                                    {item.price}
+                                </div>
+                            </div>
+                            </li>
+                        )) : <div>No Results!</div>}
+                      </ul>
+                    </div>
+                </div>
+            </form>
+          );
+      }
+
   }
-
-
- render() {
-    console.log(this.state.results);
-   return (
-     <form>
-         <div className="input-container">
-            <input
-                type="text"
-                placeholder="Search for..."
-                ref={input => this.search = input}
-                onChange={this.handleInputChange}
-            />
-            <FaSpinner icon="spinner" className={this.state.icon ? 'show spinner' : 'hide spinner'} />
-         </div>
-       <Suggestions results={this.state.results} isActive={this.state.isActive}/>
-     </form>
-   )
- }
-}
 
 export default Search
--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import { render } from '@testing-library/react';
 
 const IMAGE_PATH = '';
 const PRODUCT_PATH = '';
@@ -11,25 +9,21 @@ class Suggestions extends Component {
         this.state = {
             query: '',
             isActive: this.props.isActive,
-            results: this.props.results.data
+            results: this.props.results.data,
+            products: this.props.products
         }
     }
 
     render() {
-        // if (this.state.results) {
-        //     const results = this.state.results.data;
-        //     // console.log(results);
-        // }
-        let bad = this.state.results;
-        console.log(this.props.results + 'test');
+        console.log(this.props.products + 'test');
         return (
             <div id="container" className={this.props.isActive ? 'show' : 'hide'} >
-                {this.props.results ? (
+                {this.props.products ? (
                     <ul id="results" className={this.props.isActive ? 'show' : 'hide'}>
-                        <li><a href={PRODUCT_PATH + this.props.results.id}>{this.props.results.name}</a></li>
-                        <li>{this.props.results.description}</li>
-                        <li>{this.props.results.price}</li>
-                        <li><img src={IMAGE_PATH + this.props.results.image} /></li>
+                        <li><a href={PRODUCT_PATH + this.props.products.id}>{this.props.products.name}</a></li>
+                        <li>{this.props.products.description}</li>
+                        <li>{this.props.products.price}</li>
+                        <li><img src={IMAGE_PATH + this.props.products.image} alt={this.props.products.name} /></li>
                     </ul>
                  ) : (
                     <div className={this.props.isActive ? 'show' : 'hide'}>
@@ -41,28 +35,5 @@ class Suggestions extends Component {
       }
      
 }
-
-// const Suggestions = (props) => {
-// //   const options = props.results.map(r => (
-// //     <li key={r.id}>
-// //       {r.name}
-// //       {/* {r.description} */}
-// //     </li>
-// //   ))
-//     // render() {
-//     //     return (
-            
-//     //     )
-//     // }
-//   return <div id="container" className={props.isActive ? 'show' : 'hide'} >
-        
-//             <ul className="results">
-//                 <li><a href={PRODUCT_PATH + props.results.id}>{props.results.name}</a></li>
-//                 <li>{props.results.description}</li>
-//                 <li>{props.results.price}</li>
-//                 <li><img src={IMAGE_PATH + props.results.image} /></li>
-//             </ul>
-//          </div>
-// }
 
 export default Suggestions
